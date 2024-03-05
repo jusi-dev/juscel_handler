c3888c3fddab5c26a3a2bb413c3b75654efb7f2c
Justin Winistoerfer <winistoerfer.justin@gmx.ch>
2024-03-05 12:59:41 +0100

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,41 @@
+import express from 'express'
+require('dotenv').config()
+
+const app = express()
+
+import {
+    S3Client,
+    ListObjectsV2Command,
+    GetObjectCommand,
+} from "@aws-sdk/client-s3";
+
+const s3Client = new S3Client({});
+
+app.get("/*", async (req, res) => {
+    const host = req.hostname;
+    const id = host.split(".")[0];
+    const filePath = req.path === "/" ? "/index.html" : req.path;
+
+    const downloadCommand = new GetObjectCommand({
+        Bucket: "juscel",
+        Key: `dist/${id}${filePath}`,
+    });
+
+    const type = filePath.endsWith("html") ? "text/html" : filePath.endsWith("css") ? "text/css" : "application/javascript"
+    res.set('Content-Type', type);
+
+    s3Client.send(downloadCommand).then((data) => {
+        // @ts-ignore
+        let streamData = [];
+        // @ts-ignore
+        data.Body.on('data', (chunk) => {
+            streamData.push(chunk);
+        }).on('end', () => {
+            // @ts-ignore
+            let content = Buffer.concat(streamData);
+            res.send(content);
+        });
+    });
+})
+
+app.listen(3001);
